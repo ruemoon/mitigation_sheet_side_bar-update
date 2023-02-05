91f9713efacf6eccb2da0e8fd58954588e79d864
lastagous <from.iphone5s@gmail.com>
2023-02-05 23:56:24 +0900

    [ADD] 基本機能の実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true;
+    window.addEventListener('message', (response) => {
+      if (response.data === 'endProgress') {
+        this.isProgress = false;
+      }
+    });
+  }
+
   title = 'mitigation_sheet_side_bar';
+  isProgress = false;
+
+  contents = [
+    {
+      title: '選択ジョブ列の一括表示',
+      subTitle: 'bulk show column',
+      method: 'hiddenColumnSelectJobs',
+      image: '../assets/bulk_row.png',
+    },
+    {
+      title: 'AA行の一括非表示',
+      subTitle: 'Hide AA row',
+      method: 'hiddenAARows',
+      image: '../assets/bulk_row.png',
+    },
+    {
+      title: '軽減一覧のテキスト表示',
+      subTitle: 'perty buff list display',
+      method: 'buffTimeLinePrintText',
+      image: '../assets/bulk_row.png',
+    },
+    // {
+    //   title: "logs API key の設定",
+    //   subTitle: "setting logs api key",
+    // method: "buffTimeLinePrintText",
+    // image: "../assets/bulk_row.png",
+    // },
+    // {
+    //   title: "logs タイムラインの読み込み",
+    //   subTitle: "generate timeline from logs",
+    // method: "buffTimeLinePrintText",
+    // image: "../assets/bulk_row.png",
+    // },
+    {
+      title: 'スペスペタイムラインファイルのダウンロード',
+      subTitle: 'download xml act plugins "SpecialSpellTimer"',
+      method: 'viewSpecialSpellTimerXmlDownloadDialog',
+      image: '../assets/bulk_row.png',
+    },
+  ];
+
+  public contentClick(method: string) {
+    this.isProgress = true;
+    window?.parent?.postMessage(method, '*');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { BlockUIModule } from 'primeng/blockui';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CommonModule, BlockUIModule, ProgressSpinnerModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
