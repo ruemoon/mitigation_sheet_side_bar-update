4492dcd21644bc5b5bfd24704fd933dcb51c259c
lastagous <from.iphone5s@gmail.com>
2023-02-20 00:36:38 +0900

    [FIX] 不要コードの削除

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,43 +37,4 @@ export class AppComponent implements OnInit {
   }
 
   title = 'mitigation_sheet_side_bar';
-
-  contents = [
-    {
-      title: '選択ジョブ列の一括表示',
-      subTitle: 'bulk show column',
-      method: 'hiddenColumnSelectJobs',
-      image: '../assets/bulk_row.png',
-    },
-    {
-      title: 'AA行の一括非表示',
-      subTitle: 'Hide AA row',
-      method: 'hiddenAARows',
-      image: '../assets/bulk_row.png',
-    },
-    {
-      title: '軽減一覧のテキスト表示',
-      subTitle: 'perty buff list display',
-      method: 'buffTimeLinePrintText',
-      image: '../assets/bulk_row.png',
-    },
-    // {
-    //   title: "logs API key の設定",
-    //   subTitle: "setting logs api key",
-    // method: "buffTimeLinePrintText",
-    // image: "../assets/bulk_row.png",
-    // },
-    // {
-    //   title: "logs タイムラインの読み込み",
-    //   subTitle: "generate timeline from logs",
-    // method: "buffTimeLinePrintText",
-    // image: "../assets/bulk_row.png",
-    // },
-    {
-      title: 'スペスペタイムラインファイルのダウンロード',
-      subTitle: 'download xml act plugins "SpecialSpellTimer"',
-      method: 'viewSpecialSpellTimerXmlDownloadDialog',
-      image: '../assets/bulk_row.png',
-    },
-  ];
 }
